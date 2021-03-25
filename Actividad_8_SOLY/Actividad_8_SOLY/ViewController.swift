1555c6932a9381ecda412c533bffa524707da779
Saul <Saul>
2021-03-24 19:14:10 -0700

    Primer commit

---
+++ Actividad_8_SOLY/Actividad_8_SOLY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad_8_SOLY
+//
+//  Created by user167235 on 3/24/21.
+//  Copyright © 2021 user167235. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
