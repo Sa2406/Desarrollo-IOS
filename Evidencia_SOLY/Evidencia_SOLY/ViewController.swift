c7306e45769af93d4b4f11b6ad92413bd0de3d61
Saul <Saul>
2021-04-14 19:16:07 -0700

    Primer commit

---
+++ Evidencia_SOLY/Evidencia_SOLY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Evidencia_SOLY
+//
+//  Created by user167235 on 4/14/21.
+//  Copyright © 2021 user167235. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
